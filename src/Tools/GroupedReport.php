faa6d497d9fad27d5b6102e2deb3d4df99e05f3c
Michael Caruana <michael.caruana@internetrix.com.au>
2019-02-18 10:23:49 +1100

    Add new file

---
+++ src/Tools/GroupedReport.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Internetrix\GroupedReports;
+
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldExportButton;
+use SilverStripe\Forms\GridField\GridFieldPrintButton;
+use SilverStripe\Reports\Report;
+use SilverStripe\Security\Permission;
+use SilverStripe\Security\Security;
+
+class GroupedReport extends Report
+{
+
+    protected $searchFilters;
+
+    public function setSearchFilters($searchFilters)
+    {
+        $this->searchFilters = $searchFilters;
+        return $this->owner;
+    }
+
+    public function getSearchFilters()
+    {
+        return $this->searchFilters;
+    }
+
+    public function canView($member = null)
+    {
+        $member = Security::getCurrentUser();
+        return Permission::checkMember($member, array('CMS_ACCESS_DataObjects', 'CMS_ACCESS_ReportAdmin'));
+    }
+
+    public function getReportGridField(GridField $grid, String $filename)
+    {
+        $grid->setTitle($this->title());
+        $config = $grid->getConfig();
+        $config->removeComponentsByType(GridFieldExportButton::class);
+        $config->removeComponentsByType(GridFieldPrintButton::class);
+        $config->addComponent($print = new CustomGridFieldPrintButton('buttons-before-left'));
+        $config->addComponent($export = new CustomGridFieldExportButton('buttons-before-left'));
+        $print->setPrintColumns($this->columns());
+        $export->setExportColumns($this->columns());
+        $export->setCustomFileName($filename);
+        return $grid;
+    }
+
+}
